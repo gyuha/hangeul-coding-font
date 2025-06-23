beda8c0afb2b1827ae67d702c3567c7d2c5b7f11
gyuha <nicegyuha@gmail.com>
2025-06-23 23:40:59 +0900

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
-export default defineConfig(() => ({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || "/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  build: {
-    outDir: "docs",
-  },
-}))
+    build: {
+      outDir: "docs",
+    },
+  }
+})
